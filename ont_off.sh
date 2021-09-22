686d4b67930050847da6907b44e4bdee11ac812e
Lenin Alexander <43764718+leninalexander@users.noreply.github.com>
2021-09-22 10:53:35 -0300

    Add files via upload
    Atualização dos scripts.

--- ont_off.sh
+++ ont_off.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-count_ont=$(snmpwalk -v 2c -c ?community_snmp  ?ipOLT  .1.3.6.1.4.1.2011.6.128.1.1.2.46.1.15.$1  | grep 'INTEGER: 2' | wc -l)
-echo $count_ont;
+status_ont=$(snmpwalk -v 2c -c $1 $2 .1.3.6.1.4.1.2011.6.128.1.1.2.46.1.15.$3  | grep 'INTEGER: 2' | wc -l)
+echo $status_ont;
